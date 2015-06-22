7695d2d520350554f78d944633b8652817f20be8
David Tay <davidt@aydus.com>
2015-06-22 10:09:15 -0400

    Added thumbnail to widget

--- app/code/community/Aydus/BlogWidget/Block/Widget.php
+++ app/code/community/Aydus/BlogWidget/Block/Widget.php
@@ -32,13 +32,21 @@ class Aydus_BlogWidget_Block_Widget extends Mage_Core_Block_Template implements 
                 
                 $items = $feed->channel->item;
                 
-                foreach ($items as $i=>$item){
+                foreach ($items as $i=>$itemElement){
                     
                     if ($i>=100){
                         break;
                     }
                     
-                    $data = (array)$item;
+                    $data = (array)$itemElement;
+                    $namespaces = $itemElement->getNameSpaces(true);
+                    if (isset($namespaces['media'])){
+                        $media = $itemElement->children($namespaces['media']);
+                        $thumbnail = $media->thumbnail;
+                        $image = $thumbnail->attributes();
+                        $data['thumbnail'] = $image['url'];
+                    }
+                    
                     $item = new Varien_Object();
                     $item->setData($data);
                     
@@ -46,16 +54,6 @@ class Aydus_BlogWidget_Block_Widget extends Mage_Core_Block_Template implements 
                 }
                 
                 $orderBy = $this->getOrderBy();
-                
-                //http://stackoverflow.com/questions/20744995/how-to-sort-an-array-with-php-ignoring-the-articles-a-an-the-in-the-beginni
-                function handleArticles($str) {
-                    list($first,$rest) = explode(" ",$str." ",2);
-                       // the extra space is to prevent "undefined offset" notices
-                       // on single-word titles
-                    $validarticles = array("a","an","the");
-                    if( in_array(strtolower($first),$validarticles)) return $rest.", ".$first;
-                    return $str;
-                }
                 
                 $orderSort = function($a, $b) use ($orderBy)
                 {                    
@@ -70,7 +68,7 @@ class Aydus_BlogWidget_Block_Widget extends Mage_Core_Block_Template implements 
                         $aVal = $a->getTitle();
                         $bVal = $b->getTitle();
                         
-                        $return = strnatcasecmp(handleArticles($aVal),handleArticles($bVal));
+                        $return = strnatcasecmp($this->handleArticles($aVal),$this->handleArticles($bVal));
                     }
 
                     return $return;
@@ -84,5 +82,19 @@ class Aydus_BlogWidget_Block_Widget extends Mage_Core_Block_Template implements 
         
         return $this->_items;
     }
+    
+    /**
+     * @see http://stackoverflow.com/questions/20744995/how-to-sort-an-array-with-php-ignoring-the-articles-a-an-the-in-the-beginni
+     * @param string $str
+     * @return string
+     */
+    public function handleArticles($str) {
+        list($first,$rest) = explode(" ",$str." ",2);
+           // the extra space is to prevent "undefined offset" notices
+           // on single-word titles
+        $validarticles = array("a","an","the");
+        if( in_array(strtolower($first),$validarticles)) return $rest.", ".$first;
+        return $str;
+    } 
 	
 }
